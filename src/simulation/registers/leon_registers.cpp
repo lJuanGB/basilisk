4999c3c749e2b781000dee45d9f000f216c00eee
Mar Cols Margenet <maria.colsmargenet@colorado.edu>
2019-08-14 20:02:26 -0600

    leon module with leon_interface and leon3 calls
    Former-commit-id: 0e59aebe31cb4e4031e8fabcdfedad2473dc8e2f
    Former-commit-id: f717da8843228743bcf7fefd607695bd67967fe6

--- src/simulation/registers/leon_registers.cpp
+++ src/simulation/registers/leon_registers.cpp
@@ -1,5 +1,6 @@
 #include "leon_registers.h"
 #include <iostream>
+#include "worker_process.hpp"
 
 
 LeonRegisters::LeonRegisters()
@@ -8,6 +9,9 @@ LeonRegisters::LeonRegisters()
     this->rwTorqueMsgName_fswOut = "reactionwheel_torques";
     this->rwSpeedsMsgName_fswIn = "reactionwheel_output_states";
     this->simpleNavAttMsgName_fswIn = "simple_att_nav_output";
+    
+    this->BlackLionInterfacePresent = 0;
+    this->BlackLionConnectionInfo = "";
     return;
 }
 
@@ -36,13 +40,74 @@ void LeonRegisters::CrossInit()
 
 void LeonRegisters::UpdateState(uint64_t CurrentSimNanos)
 {
-    /* Define input message/s */
-    std::cout << "LeonRegisters::UpdateState" << std::endl;
-    //! - Zero the input buffer and read the incoming message
+    /* Read messages from FSW */
     SingleMessageHeader LocalHeader;
     RWArrayTorqueIntMsg rw_torques_buffer;
     memset(&(rw_torques_buffer), 0x0, sizeof(RWArrayTorqueIntMsg));
     SystemMessaging::GetInstance()->ReadMessage(this->rwTorqueMsgID_fswOut, &LocalHeader, sizeof(RWArrayTorqueIntMsg), reinterpret_cast<uint8_t*> (&rw_torques_buffer),moduleID);
+    /* Find associated Singleton reader and add each message as a FSW packet:
+        -- Find reader by message ID. Reader should have the pair <ID, msgName(i.e. stream_name)> and dataPackets (although we might want to preseve only the latest packet)
+     */
     
     return;
 }
+
+void LeonRegisters::Reset(uint64_t CurrentSimNanos)
+{
+    this->create_singleton_worker_process();
+    this->create_sbc_register();
+    return;
+}
+
+void LeonRegisters::processorExternalWakeup(uint64_t CurrentSimNanos)
+{
+    this->save_current_clock(CurrentSimNanos);
+    this->send_tock_msg(); // here we tock
+    this->poll_worker_sockets(); // here we tick, publish & subcribe
+    return;
+}
+
+void LeonRegisters::create_sbc_register()
+{
+    std::cout << "create_sbc_register()" << std::endl;
+    /* Define readers and writers
+     -- Readers: we need one for each fswOut msg with valid ID (those in CrossInit).
+     -- Writers: we need one for each fswIn msg as (declared in SelfInit)
+     */
+    return;
+}
+void LeonRegisters::create_singleton_worker_process()
+{
+    return;
+}
+void LeonRegisters::poll_worker_sockets()
+{
+    /* Tick. */
+    
+    /* Publish [prevTime]: pakcet/s stored in each reader go out to the world.
+     */
+    /* Subcribe[currTime]:
+     -- worker_process: parse_incoming_packet
+     -- qemu_node: route_in_packet
+     -- writer: add_bytes_to_packet & this->writer_callback_REGinbound()
+     Idea: could the writer_callback be simply a WriteMessage? the writer would have to know  [MessageID, uint64_t ClockTimeNanos, MsgSize, void *MsgPayload]
+     */
+    return;
+}
+void LeonRegisters::save_connect_info(char *connect_string)
+{
+    return;
+}
+void LeonRegisters::send_tock_msg()
+{
+    return;
+}
+void LeonRegisters::save_current_clock(uint64_t clockTime)
+{
+    return;
+}
+
+
+
+
+
--- src/simulation/registers/leon_registers.h
+++ src/simulation/registers/leon_registers.h
@@ -9,6 +9,7 @@
 #include <vector>
 //#include "zmqpp/zmqpp.hpp"
 
+
 class LeonRegisters : public SysModel {
 public:
     LeonRegisters();
@@ -16,16 +17,33 @@ public:
     void SelfInit();
     void CrossInit();
     void UpdateState(uint64_t CurrentSimNanos);
+    void Reset(uint64_t CurrentSimNanos);
+    
+private:
+    void processorExternalWakeup(uint64_t CurrentSimNanos);
+    void create_sbc_register();
+    void create_singleton_worker_process();
+    void poll_worker_sockets();
+    void save_connect_info(char *connect_string);
+    void send_tock_msg();
+    void save_current_clock(uint64_t clockTime);
+    
 public:
     uint64_t outputBufferCount;
     std::string rwTorqueMsgName_fswOut;
     std::string rwSpeedsMsgName_fswIn;
     std::string simpleNavAttMsgName_fswIn;
+    
+    std::string BlackLionConnectionInfo;
 
 private:
     int64_t rwTorqueMsgID_fswOut;
     int64_t rwSpeedsMsgID_fswIn;
     int64_t simpleNavAttMsgID_fswIn;
+    
+    int BlackLionInterfacePresent;
 };
+
+
 
 #endif /* LEON_REGISTERS_H */
