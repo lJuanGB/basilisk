3322fc3685e916b0bcc90923fed6d1e87c74115c
Leah Kiner <leki4360@colorado.edu>
2022-11-30 22:47:16 -0700

    [BKS-1999] Correctly renaming variables, deleting unused commented code, and updating the calculation of the reference PRV angle. Adding in calculation of the F2F DCM which corresponds to the required attitude transformation between the current and the desired reference frame.
    Former-commit-id: bda9855b768b65178fd373475a8701ae399484b6
    Former-commit-id: 01b6d3ab725af90a8c24cc8e2a5df8364f8c3068

--- src/fswAlgorithms/attGuidance/prescribed2DOF/prescribed2DOF.c
+++ src/fswAlgorithms/attGuidance/prescribed2DOF/prescribed2DOF.c
@@ -123,139 +123,94 @@ void Update_prescribed2DOF(Prescribed2DOFConfig *configData, uint64_t callTime, 
         double thetaDot2Ref = spinningBodyTwoDOFIn.thetaDot2; // [rad/s]
 
         /*! Convert two reference angles and rotation axes to reference PRVs */
-        double prv_F1F0_array[3];
-        double prv_MF1_array[3];
+        double prv_F1M_array[3];
+        double prv_F2F1_array[3];
         v3Normalize(configData->rotAxis1_M, configData->rotAxis1_M);
         v3Normalize(configData->rotAxis2_M, configData->rotAxis2_M);
-        v3Scale(theta1Ref, configData->rotAxis1_M, prv_F1F0_array);
-        v3Scale(theta2Ref, configData->rotAxis2_M, prv_MF1_array);
+        v3Scale(theta1Ref, configData->rotAxis1_M, prv_F1M_array);
+        v3Scale(theta2Ref, configData->rotAxis2_M, prv_F2F1_array);
 
         /*! Convert two reference PRVs to DCMs */
-        double dcm_F1F0[3][3];
-        double dcm_MF1[3][3];
-        PRV2C(prv_F1F0_array, dcm_F1F0);
-        PRV2C(prv_MF1_array, dcm_MF1);
+        double dcm_F1M[3][3];
+        double dcm_F2F1[3][3];
+        PRV2C(prv_F1M_array, dcm_F1M);
+        PRV2C(prv_F2F1_array, dcm_F2F1);
 
         /*! Combine the two computed reference DCMs to a single reference DCM */
-        double dcm_MF0[3][3];
-        m33MultM33(dcm_MF1, dcm_F1F0, dcm_MF0);
+        double dcm_F2M[3][3];
+        m33MultM33(dcm_F2F1, dcm_F1M, dcm_F2M);
+
+        /*! Compute dcm_F2F */
+        double dcm_F2F[3][3];
+        m33MultM33t(dcm_F2M, dcm_FM, dcm_F2F);
 
         /*! Combine the reference DCM to the reference PRV */
-        double prv_MF0_array[3];
-        C2PRV(dcm_MF0, prv_MF0_array);
+        double prv_F2F_array[3];
+        C2PRV(dcm_F2F, prv_F2F_array);
 
         /*! Compute the single PRV reference angle */
-        v3Normalize(prv_MF0_array, configData->rotAxis_M);
+        v3Normalize(prv_F2F_array, configData->rotAxis_M);
+        configData->phiRef = v3Dot(prv_F2F_array, configData->rotAxis_M); // [rad]
 
-        /*
-        if (configData->rotAxis_M[0] < 0 || configData->rotAxis_M[1] < 0 || configData->rotAxis_M[2] < 0)
-        {
-            configData->rotAxis_M[0] = -1 *configData->rotAxis_M[0];
-            configData->rotAxis_M[1] = -1 *configData->rotAxis_M[1];
-            configData->rotAxis_M[2] = -1 *configData->rotAxis_M[2];
-        }
-        */
-
-        configData->thetaRef = v3Dot(prv_MF0_array, configData->rotAxis_M); // [rad]
-
-        /*
-        printf("thetaRef %f \n", configData->thetaRef);
-        printf("thetaRefPrev %f \n", configData->thetaRefPrev);
-        if (abs(configData->thetaRef) == abs(configData->thetaRefPrev))
-        {
-            printf("entered \n");
-            configData->thetaRef = 2 * PI - abs(configData->thetaRef);
-        }
-        printf("thetaRef %f \n", configData->thetaRef);
-        printf("prv [%f %f %f] \n", prv_MF0_array[0], prv_MF0_array[1], prv_MF0_array[2]);
-        printf("rotAxis_M [%f %f %f] \n", configData->rotAxis_M[0], configData->rotAxis_M[1], configData->rotAxis_M[2]);
-        configData->thetaDotRef = 0.0;
-        */
-
-        /*
-        configData->thetaRef = v3Dot(prv_MF0_array, configData->rotAxis_M); // [rad]
-        printf("thetaRef %f \n", configData->thetaRef);
-
-        if (configData->thetaRef < 0)
-        {
-            printf("entered \n");
-            configData->thetaRef = 2 * PI + configData->thetaRef;
-        }
-        
-        printf("thetaRef %f \n", configData->thetaRef);
-        printf("thetaInit %f \n", configData->thetaInit);
-        */
-        
         /*! Define temporal information */
-        double convTime = sqrt(((0.5 * fabs(configData->thetaRef - configData->thetaInit)) * 8) / configData->thetaDDotMax);
+        double convTime = sqrt(fabs(configData->phiRef) * 4 / configData->thetaDDotMax); // [s]
+        
         configData->tf = configData->tInit + convTime; // [s]
-        configData->ts = configData->tInit + convTime / 2; // switch time [s]
+        configData->ts = convTime / 2 + configData->tInit; // switch time [s]
 
         // Define constants for analytic parabolas
-        configData->a = 0.5 * (configData->thetaRef - configData->thetaInit) / ((configData->ts - configData->tInit) * (configData->ts - configData->tInit)); // Constant for first parabola
-        configData->b = -0.5 * (configData->thetaRef - configData->thetaInit) / ((configData->ts - configData->tf) * (configData->ts - configData->tf)); // Constant for second parabola
+        configData->a = 0.5 * configData->phiRef / ((configData->ts - configData->tInit) * (configData->ts - configData->tInit)); // Constant for first parabola
+        configData->b = -0.5 * configData->phiRef / ((configData->ts - configData->tf) * (configData->ts - configData->tf)); // Constant for second parabola
 
         /*! Set convergence to false to keep the reset parameters */
         configData->convergence = false;
-        
-        configData->thetaRefPrev = configData->thetaRef;
     }
 
     double t = callTime*1e-9; // current time [s]
 
     /*! Define scalar module states */
-    double thetaDDot;
-    double thetaDot;
-    double theta;
+    double phiDDot;
+    double phiDot;
 
-    /*! Compute analytic scalar states: thetaDDot, thetaDot, and theta */
+    /*! Compute analytic scalar states: phiDDot, phiDot, and phi */
     if ((t < configData->ts || t == configData->ts) && configData->tf != 0)
     {
-        thetaDDot = configData->thetaDDotMax;
-        thetaDot = thetaDDot * (t - configData->tInit) + configData->thetaDotInit;
-        theta = configData->a * (t - configData->tInit) * (t - configData->tInit) + configData->thetaInit;
-        printf("1 ");
+        phiDDot = configData->thetaDDotMax;
+        phiDot = phiDDot * (t - configData->tInit);
+        configData->phi = configData->a * (t - configData->tInit) * (t - configData->tInit);
     }
     else if ( t > configData->ts && t <= configData->tf && configData->tf != 0)
     {
-        thetaDDot = -1 * configData->thetaDDotMax;
-        thetaDot = thetaDDot * (t - configData->tInit) + configData->thetaDotInit - thetaDDot * (configData->tf - configData->tInit);
-        theta = configData->b * (t - configData->tf) * (t - configData->tf) + configData->thetaRef;
-        printf("2 ");
+        phiDDot = -1 * configData->thetaDDotMax;
+        phiDot = phiDDot * (t - configData->tf );
+        configData->phi = configData->b * (t - configData->tf) * (t - configData->tf) + configData->phiRef;
     }
     else
     {
-        thetaDDot = 0.0;
-        thetaDot = configData->thetaDotRef;
-        theta = configData->thetaRef;
+        phiDDot = 0.0;
+        phiDot = 0.0;
+        configData->phi = configData->phiRef;
         configData->convergence = true;
-        printf("theta %f \n", theta);
-        printf("3 ");
+        configData->phiRefPrev = configData->phiRef;
     }
+
+    configData->thetaAccum = configData->phiRefPrev + configData->phi;
 
     /*! Determine omega_FM_F and omegaPrime_FM_F parameters */
     v3Normalize(configData->rotAxis_M, configData->rotAxis_M);
-    v3Scale(thetaDot, configData->rotAxis_M, configData->omega_FM_F);
-    v3Scale(thetaDDot, configData->rotAxis_M, configData->omegaPrime_FM_F);
+    v3Scale(phiDot, configData->rotAxis_M, configData->omega_FM_F);
+    v3Scale(phiDDot, configData->rotAxis_M, configData->omegaPrime_FM_F);
 
     /*! Determine sigma_FM, mrp from F frame to M frame */
-    double dcm_FF0[3][3];
-
     /*! Determine dcm_FF0 */
+    double dcm_FF0[3][3];
     double prv_FF0_array[3];
-    double theta_FF0 = theta - configData->thetaInit;
-    v3Scale(theta_FF0, configData->rotAxis_M, prv_FF0_array);
+    v3Scale(configData->phi, configData->rotAxis_M, prv_FF0_array);
     PRV2C(prv_FF0_array, dcm_FF0);
-
-    /*! Determine dcm_F0M */
-    double dcm_F0M[3][3];
-    double prv_F0M_array[3];
-    v3Scale(configData->thetaInit, configData->rotAxis_M, prv_F0M_array);
-    PRV2C(prv_F0M_array, dcm_F0M);
 
     /*! Determine dcm_FM */
     double dcm_FM[3][3];
-    m33MultM33(dcm_FF0, dcm_F0M, dcm_FM);
+    m33MultM33(dcm_FF0, configData->dcm_F0M, dcm_FM);
     C2MRP(dcm_FM, configData->sigma_FM);
 
     /*! Copy local variables to output message */
