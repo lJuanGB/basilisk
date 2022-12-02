66aa21f1a87387089c9bad54fd432b274ad82888
Leah Kiner <leki4360@colorado.edu>
2022-12-01 18:01:05 -0700

    [BSK-1999] Cleaning up the header file and adding comments for the variables.
    Former-commit-id: dadd0c9f987ff36b355c0a76f31c8458766194e1
    Former-commit-id: de2ac4849c610acc9837c7bb2911f3f4b85c7015

--- src/fswAlgorithms/attGuidance/prescribed2DOF/prescribed2DOF.h
+++ src/fswAlgorithms/attGuidance/prescribed2DOF/prescribed2DOF.h
@@ -32,38 +32,37 @@ typedef struct {
 
     /* User configurable variables */
     double phiDDotMax;                                         //!< Maximum angular acceleration [rad/s^2]
-    double rotAxis1_M[3];                                        //!< F-frame spin axis of prescribed body
-    double rotAxis2_F1[3];                                        //!< F-frame spin axis of prescribed body *GENERALIZE
+    double rotAxis1_M[3];                                      //!< M-frame spin axis of prescribed body
+    double rotAxis2_F1[3];                                     //!< Intermediate F1-frame spin axis of prescribed body
 
     /* Private variables */
-    double r_FM_M[3];
-    double rPrime_FM_M[3];
-    double rPrimePrime_FM_M[3];
-    double omega_FM_F[3];
-    double omegaPrime_FM_F[3];
-    double sigma_FM[3];
-    bool convergence;
-    double tInit;                                               //!< Initial time when module reset is called [s]
-    double rotAxisInit_M[3];
-    double rotAxis_M[3];
-    double phi;
-    double phiRef;
-    double phiRefPrev;
-    double phiDotRef;
-    double ts;
-    double tf;
-    double a;
-    double b;
-    double dcm_F0M[3][3];
-    double phiAccum;
+    double r_FM_M[3];                                          //!< [m] position vector of point F relative to point M in M frame components
+    double rPrime_FM_M[3];                                     //!< [m/s] B frame time derivative of position vector r_FM_M in M frame components
+    double rPrimePrime_FM_M[3];                                //!< [m/s^2] B frame time derivative of vector rPrime_FM_M in M frame components
+    double omega_FM_F[3];                                      //!< [rad/s] angular velocity vector of F frame relative to the B frame in F frame components
+    double omegaPrime_FM_F[3];                                 //!< [rad/s^2] B frame time derivative of omega_FB_F in F frame components
+    double sigma_FM[3];                                        //!< Initial MRP attitude of frame F relative to the M frame
+    bool convergence;                                          //!< Boolean variable for convergence to the desired reference attitude
+    double tInit;                                              //!< [s] Initial time when module reset is called
+    double rotAxis_M[3];                                       //!< Unit Euler axis for the reference PRV
+    double phi;                                                //!< Current PRV angle
+    double phiRef;                                             //!< Reference PRV angle (positive short rotation chosen is always)
+    double phiDotRef;                                          //!< Reference PRV angle rate
+    double phiRefAccum;                                        //!< This variable logs the accumulation of the reference PRV angles (used to determine phiAccum for plotting)
+    double phiAccum;                                           //!< This variable holds the accumulation of the current PRV angles (used to determine phiAccum for plotting)
+    double ts;                                                 //!< Switch time (halfway through the attitude maneuver)
+    double tf;                                                 //!< Final time of the reference maneuver
+    double a;                                                  //!< Upward parabolic constant for the first half of the attitude maneuver
+    double b;                                                  //!< Downward parabolic constant for the second half of the attitude maneuver
+    double dcm_F0M[3][3];                                      //!< DCM from the mount M frame to initial F0 frame of the prescribed body
 
     /* Declare module input-output interfaces */
-    SpinningBodyMsg_C    spinningBodyRef1InMsg;                     //!< Input msg for reference angles and angle rates
-    SpinningBodyMsg_C    spinningBodyRef2InMsg;                     //!< Input msg for reference angles and angle rates
-    PrescribedMotionMsg_C prescribedMotionInMsg;                            //!< Prescribed input attitude reference message
-    PrescribedMotionMsg_C prescribedMotionOutMsg;                           //!< Prescribed output attitude reference message
+    SpinningBodyMsg_C    spinningBodyRef1InMsg;                //!< Input msg for reference angles and angle rates
+    SpinningBodyMsg_C    spinningBodyRef2InMsg;                //!< Input msg for reference angles and angle rates
+    PrescribedMotionMsg_C prescribedMotionInMsg;               //!< Prescribed input attitude reference message
+    PrescribedMotionMsg_C prescribedMotionOutMsg;              //!< Prescribed output attitude reference message
 
-    BSKLogger *bskLogger;                                                   //!< BSK Logging
+    BSKLogger *bskLogger;                                      //!< BSK Logging
 
 }Prescribed2DOFConfig;
 
