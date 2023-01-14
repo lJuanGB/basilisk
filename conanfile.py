27d03d700c535d8fb776b342dd171f54a2fb200e
Patrick Kenneally <patrick.kenneally@gmail.com>
2023-01-13 17:05:16 -0700

    [EDIT} Refactor including python dependency
    This commit is not complete/contains unfinished code

--- conanfile.py
+++ conanfile.py
@@ -260,6 +260,8 @@ class BasiliskConan(ConanFile):
         cmake.definitions["BUILD_VIZINTERFACE"] = self.options.vizInterface
         cmake.definitions["EXTERNAL_MODULES_PATH"] = self.options.pathToExternalModules
         cmake.definitions["PYTHON_VERSION"] = sys.version_info.major + 0.1*sys.version_info.minor
+        # cmake.definitions["Python3_ROOT_DIR"] =
+
         cmake.parallel = True
         print(statusColor + "Configuring cmake..." + endColor)
         cmake.configure()
